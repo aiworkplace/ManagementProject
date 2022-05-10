f9a35c28e1968be89eac725a43a8c8f4a0addacc
AI Workplace <62276916+aiworkplace@users.noreply.github.com>
2022-05-10 21:53:10 +0600

    online fashion

---
+++ accounts/forms.py
@@ -0,0 +1,42 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+from .models import *
+
+class CustomerForm(ModelForm):
+	class Meta:
+		model = Customer
+		fields = '__all__'
+		exclude = ['user']
+
+class ProductForm(ModelForm):
+	class Meta:
+		model = Product
+		fields = '__all__'
+
+class OrderForm(ModelForm):
+	class Meta:
+		model = Order
+		fields = '__all__'
+
+class CuttingMasterForm(ModelForm):
+	class Meta:
+		model = CuttingMaster
+		fields = '__all__'
+
+class SewingMasterForm(ModelForm):
+	class Meta:
+		model = SewingMaster
+		fields = '__all__'
+
+class SubEmpolyForm(ModelForm):
+	class Meta:
+		model = SubEmploy
+		fields = '__all__'
+
+class CreateUserForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
---
+++ procfile.txt
@@ -0,0 +1,1 @@
+web: gunicorn CoronaAlert.wsgi --log-file -
